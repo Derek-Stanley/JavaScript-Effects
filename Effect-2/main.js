4634c3e75c95d0f50f9a1e346c793362622891e2
Ajith-Kumar-G <ajithkumarg.1.0.8@gmail.com>
2021-02-11 09:52:09 +0530

    initial

---
+++ Effect-2/main.js
@@ -0,0 +1,22 @@
+
+const root=document.querySelector('.element');
+
+class Effect2{
+
+  constructor() {
+  this.rectangleSpan = document.createElement("span");
+  this.rectangleSpan.classList.add("rectangle");
+  root.append(this.rectangleSpan);
+  this.handlePosition();
+  }
+
+  handlePosition() {
+
+  }
+  
+
+}
+
+$('.element').on('hover'),function {
+
+}
---
+++ Effect-4/main.js
@@ -0,0 +1,83 @@
+const root = document.querySelector(".bubble-style");
+
+let  innerHeight=root.offsetTop, innerWidth=root.offsetLeft;
+
+class Bubble {
+  constructor() {
+
+    this.bubbleSpan = undefined;
+    this.handleNewBubble();
+    this.color = this.randomColor();
+
+    this.posY = this.randomNumber(innerHeight);
+    this.posX = this.randomNumber(innerWidth+50);
+
+
+    this.bubbleSpan.style.top = this.posY + "%";
+    this.bubbleSpan.style.left = this.posX + "%";
+
+    this.height = this.randomNumber(25, 5);
+    this.width = this.height;
+
+    this.bubbleEnd.call(this.bubbleSpan, this.randomNumber(4000, 3000));
+  }
+
+  handleNewBubble() {
+    this.bubbleSpan = document.createElement("span");
+    this.bubbleSpan.classList.add("bubble");
+    root.append(this.bubbleSpan);
+    this.handlePosition();
+    this.bubbleSpan.addEventListener("click", this.bubbleEnd);
+  }
+
+  handlePosition() {
+    
+    this.bubbleSpan.style.backgroundColor = this.color;
+    this.bubbleSpan.style.height = this.height + "px";
+    this.bubbleSpan.style.width = this.height + "px";
+
+    this.posY = this.randomNumber(-100, 150);
+    this.posX = this.randomNumber(-100 ,150);
+
+    this.bubbleSpan.style.top = this.posY + "%";
+    this.bubbleSpan.style.left = this.posX + "%";
+
+    const randomSec = this.randomNumber(2000, 200);
+    setTimeout(this.handlePosition.bind(this), randomSec); 
+  }
+
+  randomNumber(max, min) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  randomColor() {
+    return `rgba(
+        ${this.randomNumber(0, 255)},
+        ${this.randomNumber(0, 255)},
+        ${this.randomNumber(0, 255)}, 
+        ${this.randomNumber(0.1, 1)})`;
+  }
+
+  bubbleEnd(removingTime = 0) {
+    setTimeout(
+      () => {
+        this.classList.add("bubble--bust");
+      },
+      removingTime === 0 ? removingTime : removingTime - 100
+    );
+
+    setTimeout(() => 
+    {
+        this.remove();
+        new Bubble();
+    }, removingTime);
+  }
+}
+function BubblesGo(){
+for(let i = 0; i <8;i++)
+{
+    new Bubble();
+}
+return;
+}
+
